30f5b6516d24ea4fbea10654c1c66cb992d8440c
artemesin <esin.artem5@gmail.com>
2019-08-16 13:23:10 +0300

    Route generator

--- src/main/java/visitor/VisitorRoute.java
+++ src/main/java/visitor/VisitorRoute.java
@@ -1,9 +1,15 @@
 package visitor;
 
 import floor.Floor;
+import room.Room;
+import room.RoomDAO;
+import room.RoomDAOImpl;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import static visitor.GetVisitorLocation.getVisitorLocation;
 
 public class VisitorRoute {
     private static Long tempHumanLocationX1;
@@ -11,13 +17,22 @@ public class VisitorRoute {
     private static List<Long> routX = new ArrayList<>();
     private static List<Long> routY = new ArrayList<>();
     private static Floor tempFloor;
-    private static int visitorStep = 53;
+    private static int visitorStep = 153;
+    private static RoomDAO roomDAO = RoomDAOImpl.getInstance();
+    private static List<Room> rooms;
+
+    static {
+        try {
+            rooms = roomDAO.getAllRooms();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void routeGenerator(int n){
         Long tempX = tempHumanLocationX1;
         Long tempY = tempHumanLocationY1;
         for (int i = 0; i < n; i++) {
-
             tempX = Long.parseLong(tempFloor.getMaxXSize())<(tempHumanLocationX1+visitorStep) ? tempHumanLocationX1-visitorStep : tempHumanLocationX1+visitorStep;
             tempHumanLocationX1 = tempX;
             routX.add(tempHumanLocationX1);
@@ -32,9 +47,10 @@ public class VisitorRoute {
         tempHumanLocationX1 = 10L;
         tempHumanLocationY1 = 40L;
         tempFloor = new Floor(1, 1, "1300", "600");
-        routeGenerator(3);
+        routeGenerator(20);
         for (int i = 0; i < routY.size(); i++) {
-            System.out.println(routX.get(i) + " " + routY.get(i));
+            System.out.println(routX.get(i) + " " + routY.get(i) + " roomId: " + getVisitorLocation(rooms, routX.get(i), routY.get(i)));
+
         }
 
     }
